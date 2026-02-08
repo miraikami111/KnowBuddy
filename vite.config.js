6fcb598f91b3349db1b738539ac7bdb8a33b98f1
miraikami111 <bluesky---graysky---112@outlook.jp>
2026-02-08 11:32:34 +0900

    deploy working build

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+// import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: "/KnowBuddy/",   // GitHub Pages の公開パス
+  base: "/KnowBuddy/",
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'KnowBuddy',
-        short_name: 'KnowBuddy',
-        icons: [
-          {
-            src: '/KnowBuddy/icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/KnowBuddy/icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ],
-        start_url: '/KnowBuddy/',
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#ffffff'
-      }
-    })
+    // VitePWA({
+    //   registerType: 'autoUpdate',
+    //   manifest: {
+    //     name: 'KnowBuddy',
+    //     short_name: 'KnowBuddy',
+    //     icons: [
+    //       {
+    //         src: '/KnowBuddy/icon-192.png',
+    //         sizes: '192x192',
+    //         type: 'image/png'
+    //       },
+    //       {
+    //         src: '/KnowBuddy/icon-512.png',
+    //         sizes: '512x512',
+    //         type: 'image/png'
+    //       }
+    //     ],
+    //     start_url: '/KnowBuddy/',
+    //     display: 'standalone',
+    //     background_color: '#ffffff',
+    //     theme_color: '#ffffff'
+    //   }
+    // })
   ]
 })
