e8e186ce9a35e90a66573195fd6edd741796fad6
miraikami111 <bluesky---graysky---112@outlook.jp>
2026-02-15 18:53:52 +0900

    update banner design and css import

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './App.css'
 import App from './App.jsx'
 import { registerSW } from 'virtual:pwa-register'   // ←アイコンのための
 
