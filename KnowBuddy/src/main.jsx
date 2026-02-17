7fe765218d851ef305ff58692d96d27a1f593085
miraikami111 <bluesky---graysky---112@outlook.jp>
2026-02-17 23:02:30 +0900

    disable PWA temporarily

--- KnowBuddy/src/main.jsx
+++ KnowBuddy/src/main.jsx
@@ -2,10 +2,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './App.css'
 import App from './App.jsx'
-import { registerSW } from 'virtual:pwa-register'   // ←アイコンのための
+// import { registerSW } from 'virtual:pwa-register'   // ←アイコンのための
 
 createRoot(document.getElementById('root')).render(
   <App />
 )
 
-registerSW({ immediate: true })   // アイコンのやつ
+// registerSW({ immediate: true })   // アイコンのやつ
